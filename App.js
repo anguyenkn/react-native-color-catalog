7edd1fb2c50baf2dbb472b39141e3a05d9fb1c61
Aidan Nguyen <anguyenkn@gmail.com>
2020-07-11 11:10:11 -0700

    touchable highlight

--- App.js
+++ App.js
@@ -6,12 +6,16 @@ export default function App() {
 
   return (
     <View style={[styles.page, { backgroundColor }]}>
-      <Text style={styles.button} onPress={() => setBackgroundColor('green')}>
-        Green
-      </Text>
-      <Text style={styles.button} onPress={() => setBackgroundColor('red')}>
-        Red
-      </Text>
+      <TouchableHighlight
+        style={styles.button}
+        onPress={() => setBackgroundColor('yellow')}
+        underlayColor='orange'
+      >
+        <View style={styles.row}>
+          <View style={[styles.sample, { backgroundColor: 'yellow' }]} />
+          <Text style={styles.buttonText}>Yellow</Text>
+        </View>
+      </TouchableHighlight>
     </View>
   );
 }
@@ -23,10 +27,32 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     alignItems: 'center',
   },
+  row: {
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
   button: {
     color: 'white',
     fontSize: 30,
     margin: 10,
     padding: 10,
+    borderWidth: 2,
+    borderRadius: 10,
+    borderColor: 'white',
+    alignSelf: 'stretch',
+    textAlign: 'center',
+    backgroundColor: 'rgba(0, 0, 0, 0.7)',
+  },
+  buttonText: {
+    color: 'white',
+    textAlign: 'center',
+    fontSize: 30,
+  },
+  sample: {
+    height: 20,
+    width: 20,
+    margin: 5,
+    borderRadius: 10,
+    backgroundColor: 'white',
   },
 });
