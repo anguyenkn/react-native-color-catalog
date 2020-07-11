f2304e92dc7342d2f7667d268a750096bcf9deb6
Aidan Nguyen <anguyenkn@gmail.com>
2020-07-11 10:34:30 -0700

    native components and apis examples

--- App.js
+++ App.js
@@ -1,21 +1,38 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import {
+  Text,
+  View,
+  ActivityIndicator,
+  ProgressViewIOS,
+  ProgressBarIOS,
+  Button,
+  Alert,
+  Dimensions,
+  Platform,
+} from 'react-native';
+
+const { height, width } = Dimensions.get('window');
 
 export default function App() {
+  const onButtonPress = () => {
+    Alert.alert(`${new Date().toLocaleTimeString()} button press`);
+  };
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={{ padding: 50 }}>
+      {Platform.Os === 'ios' && <ProgressViewIOS progress={0.5} />}
+      {Platform.Os === 'android' && (
+        <ProgressBarIOS
+          styleAttr='Horizontal'
+          indeterminate={false}
+          color={'blue'}
+          progress={0.5}
+        />
+      )}
+      <ActivityIndicator size='large' color='#61DBFb' />
+      <Button title='Click me' onPress={onButtonPress} />
+      <Text>OS: {Platform.OS}</Text>
+      <Text>Height: {height}</Text>
+      <Text>Width: {width}</Text>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
